bb6458f5ecff757bb9b075a1881429844d4773dc
Vlad Gheorghita <vlad.gheorghita@arnia.ro>
2018-05-07 13:36:07 +0300

    Header Component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppLayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/layout/layout.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [
+    // Components
+  ],
+  imports: [
+    // Modules
+  ],
+  providers: [
+    // Services
+  ],
+  exports: [
+    // Exported Components
+  ],
+  entryComponents: [
+    // Dynamic Components
+  ]
+})
+export class AppLayoutModule { }
